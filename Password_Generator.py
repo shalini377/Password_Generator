14fc7b1f9806ad33b3b641dae6bc2f95c1434465
SHALINI JEYAKANTHAN <84364490+shalini377@users.noreply.github.com>
2023-12-12 14:42:45 +0530

    Update Password_Generator.py

--- Password_Generator.py
+++ Password_Generator.py
@@ -5,12 +5,21 @@ output:
 your password: FB3250Lx'''
 
 import random
-password_length=int(input())
-upper_case="QWERTYUIOPLKJHGFDSAZXCVBNM"
-lower_case="qwertyuioplkjhgfdsazxcvbnm"
-symbols="!@#$%^&*/\?"
-numbers="0123456789"
-mix_password=upper_case+lower_case+symbols+numbers
-password="".join(random.sample(mix_password,password_length))
+opinion=str(input("Do you want to customize your password(y/n):"))
+password_length=int(input("Enter your password length:"))
+if opinion=='n':
+  upper_case="QWERTYUIOPLKJHGFDSAZXCVBNM"
+  lower_case="qwertyuioplkjhgfdsazxcvbnm"
+  symbols="!@#$%^&*/\?"
+  numbers="0123456789"
+  mix_password=upper_case+lower_case+symbols+numbers
+  password="".join(random.sample(mix_password,password_length))
+else:
+  name=str(input("your_name:"))
+  dob=input("DOB:")
+  print(type(dob))
+  content=str(input("Anything else to be added:"))
+  mix_password=name+dob+content
+  password="".join(random.sample(mix_password,password_length))
 print("your password:", password)
 
